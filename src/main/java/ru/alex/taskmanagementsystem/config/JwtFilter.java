bcbc0a87f683170a7202f1e07c80020520273859
R0n316 <manyakinsasha@gmail.com>
2024-08-11 11:03:54 +0300

    add access to the application via JWT token

---
+++ src/main/java/ru/alex/taskmanagementsystem/config/JwtFilter.java
@@ -0,0 +1,67 @@
+package ru.alex.taskmanagementsystem.config;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import ru.alex.taskmanagementsystem.service.UserService;
+import ru.alex.taskmanagementsystem.util.JwtUtil;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+
+    @Autowired
+    public JwtFilter(JwtUtil jwtUtil, UserService userService) {
+        this.jwtUtil = jwtUtil;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || authHeader.isBlank() || !authHeader.startsWith("Bearer ")) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,"Invalid JWT token");
+        } else {
+            String jwt = authHeader.substring(7);
+            if (jwt.isBlank()) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JWT token in Bearer Header");
+            } else {
+                try {
+                    String email = jwtUtil.validateTokenAndRetrieveClaim(jwt);
+                    UserDetails userDetails = userService.loadUserByUsername(email);
+
+                    var authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails.getUsername(),
+                            userDetails.getPassword(),
+                            userDetails.getAuthorities()
+                    );
+
+                    if(SecurityContextHolder.getContext().getAuthentication() == null){
+                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                    }
+                } catch (JWTVerificationException e) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST,"Invalid JWT token");
+                }
+            }
+            filterChain.doFilter(request,response);
+        }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getRequestURI().startsWith("/api/auth");
+    }
+}
--- src/main/java/ru/alex/taskmanagementsystem/http/controller/AuthController.java
+++ src/main/java/ru/alex/taskmanagementsystem/http/controller/AuthController.java
@@ -1,28 +1,49 @@
 package ru.alex.taskmanagementsystem.http.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import ru.alex.taskmanagementsystem.dto.AuthDto;
 import ru.alex.taskmanagementsystem.service.UserService;
+import ru.alex.taskmanagementsystem.util.JwtUtil;
 
-import static org.springframework.http.HttpStatus.CREATED;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
     private final UserService userService;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, JwtUtil jwtUtil,
+                          AuthenticationManager authenticationManager) {
         this.userService = userService;
+        this.jwtUtil = jwtUtil;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<HttpStatus> register(@RequestBody AuthDto user){
+    public Map<String, String> performRegistration(@RequestBody AuthDto user) {
+//        TODO заменить Map<> на сущность для ответа
         userService.register(user);
-        return new ResponseEntity<>(CREATED);
+        String token = jwtUtil.generateToken(user.email());
+        return Map.of("jwt-token", token);
+    }
+
+//    TODO заменить везде Map<> на нормальный ответ
+    @PostMapping("/login")
+    public Map<String, String> performLogin(@RequestBody AuthDto user) {
+        var authToken = new UsernamePasswordAuthenticationToken(user.email(), user.password());
+        try {
+            authenticationManager.authenticate(authToken);
+        } catch (BadCredentialsException e) {
+            return Map.of("message","Incorrect credentials");
+        }
+        String token = jwtUtil.generateToken(user.email());
+        return Map.of("jwt-token",token);
     }
 }
